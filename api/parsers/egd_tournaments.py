3001f20e01ba842814dd172cec06b5b1dc40130c
jumpman24 <alexander.gilyazov@gmail.com>
2017-01-16 17:53:41 +0200

    Updated api for updating TournamentPlayer table

--- api/parsers/egd_tournaments.py
+++ api/parsers/egd_tournaments.py
@@ -1,23 +1,28 @@
 from UGD.models.tournaments import Tournament
 from UGD.models.players import Player
-
+from UGD.models.games import TournamentPlayer
+from UGD.models.ranks import Rank
 
 def get_tournament_list(request):
     file = open('/all.hst')
-    count = 0
-    tournament_list = set()
     for line in file:
-        if len(line.split()) == 11 and line.split()[3] == 'UA' and line.split()[0] in [player.egd_pin for player in Player.objects.all()]:
-            # print(line.split())
-            tournament_list.add(line.split()[6])
-            print(line.split()[6])
-            Tournament.objects.update_or_create(egd_name=line.split()[6])
-            # count += 1
-        # if count > 5:
-        #     file.close()
-        #     break
-        count += 1
-        print(count)
+        if len(line.split()) == 11:
+            if line.split()[6] in [tournament.egd_code for tournament in Tournament.objects.all()]:
+                if line.split()[0] in [player.egd_pin for player in Player.objects.all()]:
+                    tournament = Tournament.objects.get(egd_code=line.split()[6])
+                    player = Player.objects.get(egd_pin=line.split()[0])
+                    if Rank.objects.get(egd_grade=line.split()[5]):
+                        rank = Rank.objects.get(egd_grade=line.split()[5])
+                    else:
+                        rank = None
+                    TournamentPlayer.objects.update_or_create(
+                        player=player,
+                        tournament=tournament,
+                        defaults={
+                            'rank': rank,
+                            'egd_rating_start': line.split()[9],
+                            'egd_rating_finish': line.split()[10]
+                        }
+                    )
+                    print(str(player.__str__())+' @ '+str(tournament.__str__())+' - '+str(line.split()[9])+' --> '+str(line.split()[10]))
     file.close()
-    print([line for line in tournament_list])
-    print(Tournament.objects.all())
