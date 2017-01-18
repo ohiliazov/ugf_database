a96d29bff09815891564689474ce11c287e89caf
jumpman24 <alexander.gilyazov@gmail.com>
2017-01-18 13:34:05 +0200

    Updated parser for tournament list

--- api/parsers/egd_tournaments.py
+++ api/parsers/egd_tournaments.py
@@ -2,6 +2,8 @@ from UGD.models.tournaments import Tournament
 from UGD.models.players import Player
 from UGD.models.games import TournamentPlayer
 from UGD.models.ranks import Rank
+from UGD.models.clubs import Country, City
+import datetime
 import re
 
 
@@ -35,8 +37,21 @@ def tournaments_upload(request):
     for line in file:
         if len(line.split(maxsplit=6)) == 7 and line.split(maxsplit=6)[0] in [tournament.egd_code for tournament in Tournament.objects.all()]:
             tournament = Tournament.objects.get(egd_code=line.split(maxsplit=6)[0])
-            reg = re.match(r".(?P<date>[\d]+)+", line.split(maxsplit=6)[0])
-            print(reg.group('date'))
+            reg_date = re.match(r".(?P<date>[\d]+)+", line.split(maxsplit=6)[0])
+            tournament.date_begin = datetime.datetime.strptime(reg_date.group('date'), "%y%m%d").date()
+            print(tournament.date_begin)
+            if tournament.date_begin.weekday() == 0:
+                temp_weekday = 7
+            else:
+                temp_weekday = tournament.date_begin.weekday()
+            tournament.date_end = tournament.date_begin + datetime.timedelta(6 - temp_weekday)
+            print(tournament.date_end)
+            reg_country = re.match(r"\((?P<country>[\w]+)+\)", line.split(maxsplit=6)[4])
+            reg_city = re.match(r"(?P<city>[\w\s-]+)+", line.split(maxsplit=6)[5])
+            country = Country.objects.update_or_create(egd_name=reg_country.group('country'))
+            city = City.objects.update_or_create(country=country[0], egd_name=reg_city.group('city'))
+            print(city[0], country[0])
+            tournament.city = city[0]
             tournament.egd_class = line.split(maxsplit=6)[1]
             tournament.egd_name = line.split(maxsplit=6)[6]
             tournament.save()
