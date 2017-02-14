3374355605ef01f26ce95585faee234e9675e805
jumpman24 <alexander.gilyazov@gmail.com>
2017-02-14 17:56:47 +0200

    0.2.1. Added navigation + more modern style

--- UGD/static/UGD/js/rating_list_filter.js
+++ UGD/static/UGD/js/rating_list_filter.js
@@ -1,18 +1,27 @@
 $(".form-control").keyup(function() {
     var input = document.getElementsByClassName("form-control");
+    // Записываем значения ввода
     var full_name_value = input[0].value.toLowerCase();
     var city_value = input[1].value.toLowerCase();
+    // Выбираем все строки таблиц
     var row = $('table.ugd_table tr');
+    // Цикл по каждому ряду
     row.each(function() {
+        // Выбираем ячейки имени и города
         var full_name = $(this).children("td.full_name");
         var city = $(this).children("td.city");
+        // Проверяем есть ли в имени и городе нужные строки ввода
         if ((full_name.text().toLowerCase().indexOf(full_name_value)) > -1 && city.text().toLowerCase().indexOf(city_value) > -1) {
+            // Отображаем, если оба поля содержат значение
             full_name.parent().show();
+            // Отображаем таблицу если она была скрыта
             full_name.closest('div').parent().show();
         } else {
+            // Скрываем строку, если не совпадает хотя бы одно из полей
             full_name.parent().hide();
         }
     });
+
     var tables = $('table.ugd_table');
     tables.each(function() {
         var rows_count = ($(this).find('tr')).length;
@@ -23,4 +32,22 @@ $(".form-control").keyup(function() {
             $(this).closest('div').parent().show();
         }
     });
+});
+$(document).ready(function () {
+   var cell = $("table td.rank");
+   cell.each(function () {
+       if ($(this).text().match(/^\d про дан/)) {
+           $(this).closest("tr").toggleClass("gold_belt");
+       } else if ($(this).text().match(/^\d дан/)) {
+           $(this).closest("tr").toggleClass("black_belt");
+       } else if ($(this).text().match(/^(1|2|3|4) кю/)) {
+           $(this).closest("tr").toggleClass("blue_belt");
+       } else if ($(this).text().match(/^(5|6|7|8|9) кю/)) {
+           $(this).closest("tr").toggleClass("green_belt");
+       } else if ($(this).text().match(/^1(0|1|2|3|4) кю/)) {
+           $(this).closest("tr").toggleClass("yellow_belt");
+       } else if ($(this).text().match(/^(1(5|6|7|8|9)|20) кю/)) {
+           $(this).closest("tr").toggleClass("white_belt");
+       }
+   });
 });
--- UGD/views/rating_list.py
+++ UGD/views/rating_list.py
@@ -1,21 +1,17 @@
-from django_tables2 import MultiTableMixin
+from django_tables2 import SingleTableMixin
 from ..tables.rating_list import PlayerTable
 from ..models.players import Player
 from django.views.generic import TemplateView
 
 
 # Create your views here.
-class RatingListView(MultiTableMixin, TemplateView):
+class RatingListView(SingleTableMixin, TemplateView):
     """
     Рейтинг-лист УФГО
     """
     table_pagination = False
     template_name = 'UGD/rating_list.html'
-    tables = [
-        PlayerTable(Player.objects.filter(rank__gte=28).order_by('-rating', 'last_name')),
-        PlayerTable(Player.objects.filter(active=True, rank__lt=27, rank__gte=21).order_by('-rating', 'last_name')),
-        PlayerTable(Player.objects.filter(active=True, rank__lt=21, rank__gte=17).order_by('-rating', 'last_name')),
-        PlayerTable(Player.objects.filter(active=True, rank__lt=17, rank__gte=12).order_by('-rating', 'last_name')),
-        PlayerTable(Player.objects.filter(active=True, rank__lt=12, rank__gte=7).order_by('-rating', 'last_name')),
-        PlayerTable(Player.objects.filter(active=True, rank__lt=7).order_by('-rating', 'last_name'))
-    ]
+    table_class = PlayerTable
+
+    def get_table_data(self):
+        return Player.objects.filter(active=True).order_by('-rating')
