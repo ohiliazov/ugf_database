a4a62775f66b5edc9de863c59b8aaf70a68fcec0
Maks Ushakov <ush.maksim@gmail.com>
2017-03-10 21:56:04 +0200

    Release ajax sugges feature in rating calculator

--- UGD/static/UGD/js/rating_calculator.js
+++ UGD/static/UGD/js/rating_calculator.js
@@ -1,58 +1,93 @@
-/* if you in input, every time when you press key call function suggestOn() */  
-$('#suggest_form').delegate('input', 'keyup', suggestOn);
+/**
+ * Module Suggest
+ *
+ */
+;$.fn.suggest  = function(option) {
+	var opts = $.extend( {}, $.fn.suggest.default, option );
+	var parent = $( this );
+	var data_type = 'input[data-type="' + opts.data_type +'"]';
+	/* if you in input, every time when you press key call function suggestOn() */  
+	$(parent).on('keyup', data_type, suggestOn);
 
-/* if input lost focus delete suggest block */
-$('#suggest_form').on('change',$('input').parent(), suggestLost);
+	/* if input lost focus delete suggest block */
+	$(parent).on('change',$(data_type).parent(), suggestLost);
 
-/* click inside suggest block will call function complete */
-$('#suggest_form').delegate('#suggest div', 'click', complete);
+	/* click inside suggest block will call function complete */
+	$(parent).on('click','#' + opts.suggest_block + ' div',complete);
 
-
-/* Create Suggest Block */
-function suggestOn(event) {
-	suggestOff();
-	$('<div id="suggest"></div>')
-		.attr('data-target', $(this).attr('data-target'))
-		.appendTo($(this).parent());
-	var request = $(this).val();
-	getPlayersList(request);
-}
-
-function suggestLost (event) {
-	setTimeout(suggestOff, 800);
-}
-
-/* Destroy suggest Block */
-function suggestOff() {
-	$('#suggest').remove();
-}
-
-/* Write rating in target input */
-function complete (event) {
-	var text = $(this).text();
-	var target = $('#suggest').attr('data-target');
-	var rating = $(this).attr('data-rating');
-	$('#suggest').parent().find('input').val(text);
-	$('#' + target).val(rating);
-	suggestOff();
-}
-
-/* Fill Suggest block */
-function suggestRender(data) {
-	for(var item in data) {
-		var suggestString = data[item][0] + " (" + data[item][1] + ")";
-		$('<div></div>').attr('data-rating', data[item][1])
-						.html(suggestString)
-						.appendTo('#suggest');
+	/* Create Suggest Block */
+	function suggestOn(event) {
+		suggestOff();
+		$('<div id="' + opts.suggest_block + '"></div>')
+			.attr(opts.target_by, $(this).attr(opts.target_by))
+			.appendTo($(this).parent());
+		var request = $(this).val();
+		getPlayersList(request);
 	}
-}
 
-/* Get players from server */
-function getPlayersList (query) {
-		$.getJSON('/api/json/player_list/?full_name=' + query)
-			.success(suggestRender)
-			.error(function(err){
-				console.log(err);
-				alert('Не удалось !!');
-			});
-}
+	function suggestLost (event) {
+		setTimeout(suggestOff, 800);
+	}
+
+	/* Destroy suggest Block */
+	function suggestOff() {
+		$('#suggest').remove();
+	}
+
+	/* Write rating in target input */
+	function complete (event) {
+		var text = $(this).text();
+		var target = $('#' + opts.suggest_block).attr(opts.target_by);
+		var rating = $(this).attr('data-rating');
+		$('#' + opts.suggest_block).parent().find('input').val(text);
+		$('#' + target).val(rating);
+		suggestOff();
+	}
+
+	/* Fill Suggest block */
+	function suggestRender(data) {
+		for(var item in data) {
+			var suggestString = data[item][0] + " (" + data[item][1] + ")";
+			$('<div></div>').attr('data-rating', data[item][1])
+							.html(suggestString)
+							.appendTo('#' + opts.suggest_block);
+		}
+	}
+
+	/* Get players from server */
+	function getPlayersList (query) {
+			$.getJSON('/api/json/player_list/?full_name=' + query)
+				.success(suggestRender)
+				.error(function(err){
+					console.log(err);
+					alert('Не удалось !!');
+				});
+	}
+};
+
+$.fn.suggest.default = {
+	/*parent: form*/
+	data_type: "suggest",
+	suggest_block: 'suggest',
+	target_by: 'data-target',
+	attribute: 'data-rating',
+	link: '/'
+};
+
+$(document).ready(function () {
+	$('#rating_form').suggest({
+		link: '/api/json/player_list/?full_name=' 
+	});
+
+	$('#rating_form').on('click','.suggest-trigger',showSuggestInput);
+
+	function showSuggestInput () {
+		var target = $(this).parent().find('.suggest-input');
+		if (!this.checked) {
+			target.hide();
+		} else {
+			target.show();
+		}
+	}
+});
+
