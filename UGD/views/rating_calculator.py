3eb88e77141674986234a50e9fb44ebfa19bd5fe
jumpman24 <alexander.gilyazov@gmail.com>
2017-03-10 10:21:15 +0200

    0.4.2 Fixed calculator

--- UGD/views/rating_calculator.py
+++ UGD/views/rating_calculator.py
@@ -13,18 +13,18 @@ class RatingCalculatorView(TemplateView):
             first_rating = float(self.request.GET['first_rating'])
             second_rating = float(self.request.GET['second_rating'])
             result = float(self.request.GET['result'])
-            context['new_rating_1'] = rate_calc_func.new_rating(
-                first_rating, second_rating, result)
-            context['new_rating_1a'] = rate_calc_func.new_rating(
-                first_rating, second_rating, (1-result))
-            context['new_rating_2'] = rate_calc_func.new_rating(
-                second_rating, first_rating, (1-result))
-            context['new_rating_2a'] = rate_calc_func.new_rating(
-                second_rating, first_rating, result)
-            context['con_param_1'] = rate_calc_func.con(first_rating)
-            context['con_param_2'] = rate_calc_func.con(second_rating)
-            context['a_param_1'] = rate_calc_func.a_param(first_rating)
-            context['a_param_2'] = rate_calc_func.a_param(second_rating)
+            context['new_rating_1'] = round(rate_calc_func.new_rating(
+                first_rating, second_rating, result), 2)
+            context['new_rating_1a'] = round(rate_calc_func.new_rating(
+                first_rating, second_rating, (1 - result)), 2)
+            context['new_rating_2'] = round(rate_calc_func.new_rating(
+                second_rating, first_rating, (1 - result)), 2)
+            context['new_rating_2a'] = round(rate_calc_func.new_rating(
+                second_rating, first_rating, result), 2)
+            context['con_param_1'] = round(rate_calc_func.con(first_rating), 2)
+            context['con_param_2'] = round(rate_calc_func.con(second_rating), 2)
+            context['a_param_1'] = round(rate_calc_func.a_param(first_rating), 2)
+            context['a_param_2'] = round(rate_calc_func.a_param(second_rating), 2)
             context['win_exp_1'] = round(rate_calc_func.winning_expectancy(first_rating, second_rating), 2)
             context['win_exp_2'] = round(rate_calc_func.winning_expectancy(second_rating, first_rating), 2)
         except KeyError:
